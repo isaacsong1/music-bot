07a6d3f20653dd5b154b185d0b02212b9a63ed3b
Isaac Song <isaacsongs10@gmail.com>
2024-06-25 14:34:58 -0700

    add channel joining logic

--- commands/utility/join.js
+++ commands/utility/join.js
@@ -1,11 +1,19 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, ChannelType } = require('discord.js');
 
 module.exports = {
     cooldown: 5,
 	data: new SlashCommandBuilder()
 		.setName('join')
-		.setDescription('Joins a Voice Channel'),
-	async execute(interaction) {
-		// await interaction.reply({ content: 'Secret Pong!', ephemeral: true });
-	},
+		.setDescription('Joins a Voice Channel')
+        .addChannelOptions((option) => 
+            option
+                .setName('channel')
+                .setDescription('The channel to join')
+                .setRequired(true)
+                .addChannelTypes(ChannelType.GuildVoice)
+        )
+        .toJSON(),
+	// async execute(interaction) {
+	// 	await interaction.reply({ content: 'Secret Pong!', ephemeral: true });
+	// },
 };
