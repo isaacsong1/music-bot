ccd71d50d0d697c0980c0205eba49f1f01142d0c
Isaac Song <isaacsongs10@gmail.com>
2024-06-26 15:54:48 -0700

    test new slash commands

--- commands/utility/join.js
+++ commands/utility/join.js
@@ -1,18 +1,18 @@
-const { SlashCommandBuilder, ChannelType } = require('discord.js');
+// const { SlashCommandBuilder, ChannelType } = require('discord.js'); 
+const { SlashCommandBuilder } = require('discord.js'); 
 
 module.exports = {
-    cooldown: 5,
 	data: new SlashCommandBuilder()
 		.setName('join')
-		.setDescription('Joins a Voice Channel')
-        .addChannelOption((option) => 
-            option
-                .setName('channel')
-                .setDescription('The channel to join')
-                .setRequired(true)
-                .addChannelTypes(ChannelType.GuildVoice)
-        )
-        .toJSON(),
+		.setDescription('Joins a Voice Channel'),
+        // .addChannelOption((option) => 
+        //     option
+        //         .setName('channel')
+        //         .setDescription('The channel to join')
+        //         .setRequired(true)
+        //         .addChannelTypes(ChannelType.GuildVoice)
+        // )
+        // .toJSON(),
 	async execute(interaction) {
 		await interaction.reply('This is the join command.');
 	},
